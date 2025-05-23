67a31f137e4e59c393ee172b753aa2404f93d8b3
KarimovMurodilla <karimovmurodilla15@gmail.com>
2025-05-23 17:05:46 +0500

    some changes

--- leads/admin.py
+++ leads/admin.py
@@ -7,15 +7,3 @@ class LeadAdmin(admin.ModelAdmin):
     list_filter = ['status', 'created_at']
     search_fields = ['first_name', 'last_name', 'email']
     readonly_fields = ['created_at', 'updated_at']
-
-# ==== authentication/urls.py ====
-from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
